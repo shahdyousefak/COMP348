1c3f6333142420ae95f6fa8b0f21276b993b1854
shahdyousefak <67276189+shahdyousefak@users.noreply.github.com>
2020-10-05 15:05:22 -0400

    Update planets.pl

--- Prolog/planets.pl
+++ Prolog/planets.pl
@@ -1,1 +1,69 @@
+object(sun).
+object(mercury).
+object(venus).
+object(earth).
+object(mars).
+object(jupiter).
+object(saturn).
+object(uranus).
+object(neptune).
+object(pluto).
+object(moon).
+object(deimos).
+object(phobos).
+object(arche).
+object(callisto).
+object(europa).
+object(io).
+object(themisto).
+object(atlas).
+object(calypso).
+object(helene).
+object(desdemona).
+object(titania).
+object(despina).
+object(galatea).
+object(larissa).
+object(thalassa).
 
+mass(mercury, 0.33).  %% mass in 10^24 KG
+mass(venus, 4.87).
+mass(earth, 5.98).
+mass(mars, 0.64).
+mass(jupiter, 1900).
+mass(saturn, 569).
+mass(uranus, 569).
+mass(neptune, 86.8).
+mass(pluto, 0.02).
+
+orbits(mercury, sun).
+orbits(venus, sun).
+orbits(earth, sun).
+orbits(mars, sun).
+orbits(jupiter, sun).
+orbits(saturn, sun).
+orbits(uranus, sun).
+orbits(neptune, sun).
+orbits(pluto, sun).
+orbits(moon, earth).
+orbits(deimos, mars).
+orbits(phobos, mars).
+orbits(arche, jupiter).
+orbits(callisto, jupiter).
+orbits(europa, jupiter).
+orbits(io, jupiter).
+orbits(themisto, jupiter).
+orbits(atlas, saturn).
+orbits(calypso, saturn).
+orbits(helene, saturn).
+orbits(desdemona, uranus).
+orbits(titania, uranus).
+orbits(despina, neptune).
+orbits(galatea, neptune).
+orbits(larissa, neptune).
+orbits(thalassa, neptune).
+
+planet(P):- object(P), mass(P, Mass), Mass >= 0.3, orbits(P, sun). 
+satellite(S) :- object(S), orbits(S, P), planet(P).
+
+/* ground query: satellite(phobos).*/
